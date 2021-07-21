de003f5bed8242090e09b714587e74813efadfd2
aadithp <ci19639@bristol.ac.uk>
2021-07-21 15:05:07 +0100

    HTTP Function
    Created a simple HTTP function that capitalises all letters in a message.

---
+++ HTTPFunction/cloudFunction/cloudFunction/src/main/java/com/example/cloudFunction/CloudFunctionApplication.java
@@ -0,0 +1,20 @@
+package com.example.cloudFunction;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFunctionApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> function() {
+		return String::toUpperCase;
+	}
+}
